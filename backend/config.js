8c9ebb217b6724041e7120e32a84ca17465dd4ae
legendij <johnsonisaiah91@gmail.com>
2025-12-14 23:07:01 -0500

    Update frontend and backend configs for ALB DNS and CORS

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  CORS_ORIGIN: "http:devops-challenge-alb-151485846.us-east-1.elb.amazonaws.com>"
+  CORS_ORIGIN: "http://devops-challenge-alb-151485846.us-east-1.elb.amazonaws.com"
 };
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  backendUrl: "http://devops-challenge-alb-151485846.us-east-1.elb.amazonaws.com/api"
+  backendUrl: "http://devops-challenge-alb-151485846.us-east-1.elb.amazonaws.com/api/guid"
 };
 
 export default config;
